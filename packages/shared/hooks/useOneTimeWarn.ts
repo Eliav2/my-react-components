9a98726daa0c88ebdfef5aac4d01b9e7e22840b3
eliav2 <louski.a@gmail.com>
2022-07-19 00:54:37 +0300

    update

--- packages/shared/hooks/useOneTimeWarn.ts
+++ packages/shared/hooks/useOneTimeWarn.ts
@@ -4,6 +4,7 @@ import isEqual from "react-fast-compare";
 export const useOneTimeWarn = (intro = "", logFunc = console.error) => {
   const warnsHistory = useRef<any[][]>([]);
   const warn = (...warns) => {
+    // console.log("useOneTimeWarn", warns);
     if (!warnsHistory.current.find((w) => isEqual(w, warns))) {
       logFunc(intro, ...warns);
       warnsHistory.current.push(warns);
